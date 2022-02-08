149aa1fb24e002f987684dabcb9ae6322af3ec6c
vlad10100 <vbucaralde@gmail.com>
2022-02-09 06:57:55 +0800

    comments

--- app.js
+++ app.js
@@ -19,33 +19,10 @@ const messageDisplay = document.querySelector(".message-container")
 const wordle = 'VLADY'
 
 const keys = [
-    'Q',
-    'W',
-    'E',
-    'R',
-    'T',
-    'Y',
-    'U',
-    'I',
-    'O',
-    'P',
-    'A',
-    'S',
-    'D',
-    'F',
-    'G',
-    'H',
-    'J',
-    'K',
-    'L',
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
     'ENTER',
-    'Z',
-    'X',
-    'C',
-    'V',
-    'B',
-    'N',
-    'M',
+    'Z', 'X', 'C', 'V', 'B', 'N', 'M',
     '<<',
 ]
 
@@ -58,39 +35,89 @@ const guessRows = [
     ['', '', '', '', ''],
 ]
 
+
+
+
 let currentRow = 0
-let currentTile = 0
+let currentTile = 0             // let --->>> can be reassigned
 let isGameOver = false
 
 
 
+
+
+
+
+
+
+// create a row
+// create tiles inside the row
+// move on and repeat until max rows is reached.
 guessRows.forEach((guessRow, guessRowIndex) => {
+
+    // create a div that contains all the tiles in a row.
     const rowElement = document.createElement('div')
     rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
+    // <div id="guessRow-(guessRowIndex)"><div> 
+    // example: guessRowIndex = 1
+    // <div id="guessRow-1"><div> 
+
+
+    // Inside the guessRow
     guessRow.forEach((guess, guessIndex) => {
+        // create a div that contains a single tile.
         const tileElement = document.createElement('div')
+
         tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
         tileElement.classList.add('tile')
+
+        // After creating a TILE  it needs to append and create a new unitl the guessIndex is reached.
         rowElement.append(tileElement)
     })
+
+    // After creating a full row, its time to append and move on to the next row.
     tileDisplay.append(rowElement)
+
 })
 
 
 
 
 
-
+// key = qwetyuiop...
+// loop thru all the keys
 keys.forEach(key => {
+    // create a button (element) for each keys
     const buttonElement = document.createElement('button')
+
+    // Assign each key to be the value of the button
+    // example...  <button>KEY</button>
     buttonElement.textContent = key
+    // Create an id for each key, id=key
     buttonElement.setAttribute('id', key)
+
+    // when clicked, the click will be handled in the handleClick function
     buttonElement.addEventListener('click', () => handleClick(key))
+    // append and move to the next key
     keyboard.append(buttonElement)
 })
 
+
+
+
+
+
+// Arrow function
+// xyzFunction(parameter) 
+
+// const xyzFunction = (parameter) => {
+//     do something with the passed parameter
+// }
+
+
 const handleClick = (letter) => {
-    if (!isGameOver) {
+    // !isGameOver **equal** (isGameOver == false)
+    if (isGameOver == false) {
         console.log('clicked', letter)
         if (letter === '<<') {
             deleteLetter()
@@ -99,16 +126,16 @@ const handleClick = (letter) => {
         }
 
         if (letter === 'ENTER') {
-            console.log('check the row')
             checkRow()
+            console.log('check the row')
             return
         }
 
         addLetter(letter)
     }
-
-    
 }
+
+
 
 const addLetter = (letter) => {
     if (currentTile < 5 && currentRow < 6){
@@ -117,7 +144,7 @@ const addLetter = (letter) => {
         tile.textContent = letter
         guessRows[currentRow][currentTile] = letter
         tile.setAttribute('data', letter)
-        currentTile++
+        currentTile++ // move to the next tile
         console.log('guessRows', guessRows)
     }
     
@@ -125,17 +152,17 @@ const addLetter = (letter) => {
 
 const deleteLetter = () => {
     if (currentTile > 0) {
-        currentTile--
+        currentTile-- // move back first and then replace with blank string
         const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
         tile.textContent = ''
-        guessRows[currentRow][currentTile] = ''
-        tile.setAttribute('data', '')
+        guessRows[currentRow][currentTile] = '' // to print the matrix
+        tile.setAttribute('data', '')           // set the attribute to blank string
     }
 
 }
 
 const checkRow = () => {
-    const guess = guessRows[currentRow].join('')
+    const guess = guessRows[currentRow].join('') // combine the list to form a string
     console.log('guess: ', guess)
     
     if (currentTile > 4) {
@@ -180,18 +207,30 @@ const flipTile = (tile) => {
     const rowTiles = document.querySelector('#guessRow-' + currentRow).childNodes
     let checkWordle = wordle
     const guess = []
+    
 
+
+    // guess has a letter and color
+
+
+    // set to color grey 
     rowTiles.forEach(tile => {
         guess.push({letter:tile.getAttribute('data'), color: 'grey-overLay'})
+
     })
 
+
+    // set to color green
     guess.forEach((guess, index) => {
+        console.log(guess)
         if (guess.letter == wordle[index]){
             guess.color = 'green-overLay'
             checkWordle = checkWordle.replace(guess.letter, '')
-
         }
     })
+
+
+    // set to color yellow
     guess.forEach(guess => {
         if(checkWordle.includes(guess.letter)) {
             guess.color = 'yellow-overLay'
@@ -208,26 +247,6 @@ const flipTile = (tile) => {
             tile.classList.add('flip')
             tile.classList.add(guess[index].color)
             addColorToKey(guess[index].letter, guess[index].color)
-            // if (dataLetter == wordle[index]){
-            //     tile.classList.add('green-overLay')
-
-            //     addColorToKey(dataLetter,'green-overLay')
-
-            // }
-            // else if (wordle.includes(dataLetter)){
-            //     tile.classList.add('yellow-overLay')
-
-            //     addColorToKey(dataLetter,'yellow-overLay')
-
-            // }
-            // else {
-            //     tile.classList.add('grey-overLay')
-
-            //     addColorToKey(dataLetter,'grey-overLay')
-
-            // }
         },500*index)
-
-
     })
 }
